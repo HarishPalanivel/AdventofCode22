cb5a2fb3990bc352fcc41b35ba4aa45116ba52ef
Harish V P <65412053+HarishPalanivel@users.noreply.github.com>
2022-12-04 18:39:17 +0530

    Beautify code.

--- day1/main.py
+++ day1/main.py
@@ -1,4 +1,9 @@
-with open("day1\\input.txt", "r") as f:
+"""
+https://adventofcode.com/2022/day/1
+"""
+
+# Calculate calories carried by elves.
+with open("input.txt", "r") as f:
     total_calorie_list = []
     calorie_of_elf: int = 0
     for line in f:
@@ -7,5 +12,9 @@ with open("day1\\input.txt", "r") as f:
             calorie_of_elf = 0
         else:
             calorie_of_elf += int(line)
-    print(max(total_calorie_list))
-    print(sum(sorted(total_calorie_list, reverse=True)[:3]))
+
+# Solution for 1st question.
+print(max(total_calorie_list)) # 71023
+
+#Solution for 2nd question.
+print(sum(sorted(total_calorie_list, reverse=True)[:3])) # 206289
--- day2/main.py
+++ day2/main.py
@@ -1,15 +1,24 @@
+"""
+https://adventofcode.com/2022/day/2
+"""
+
+
+def points_cal(path: str, predicted_result: dict):
+    """Calculates the points based on the input and the possible outcomes of each play."""
+    points:int = 0
+    with open(path, "r") as f:
+        for line in f:
+            points += predicted_result[line.strip()]
+    return points
+
+
+# Possible outcomes for each play and the points awared, for both questions.
 rps: dict = {"A X": 4, "A Y" : 8, "A Z" : 3, "B X": 1, "B Y": 5, "B Z" : 9, "C X": 7, "C Y": 2, "C Z" : 6}
 rps2: dict = {"A X": 3, "A Y" : 4, "A Z" : 8, "B X": 1, "B Y": 5, "B Z" : 9, "C X": 2, "C Y": 6, "C Z" : 7}
-points: int = 0
-points2: int = 0
-with open("input.txt", "r") as f:
-    for line in f:
-        filter = line.strip()
-        points += rps[filter]
-print(points)
 
-with open("input.txt", "r") as f:
-    for line in f:
-        filter = line.strip()
-        points2 += rps2[filter]
-print(points2)
+
+#Solution for 1st question.
+print(points_cal("input.txt", rps)) # 8890
+
+#Solution for 2nd question.
+print(points_cal("input.txt", rps2)) # 10238
