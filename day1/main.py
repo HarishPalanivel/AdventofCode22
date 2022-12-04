d8808120ad2d84183b015ef0e502814ebd3b968b
Harish V P <65412053+HarishPalanivel@users.noreply.github.com>
2022-12-04 18:36:09 +0530

    Revert "Day 4"
    This reverts commit 6eaf01c2ae9edc8bb8ebca43a9575c4e2cbaed5f.

--- day1/main.py
+++ day1/main.py
@@ -1,9 +1,4 @@
-"""
-https://adventofcode.com/2022/day/1
-"""
-
-# Calculate calories carried by elves.
-with open("input.txt", "r") as f:
+with open("day1\\input.txt", "r") as f:
     total_calorie_list = []
     calorie_of_elf: int = 0
     for line in f:
@@ -12,9 +7,5 @@ with open("input.txt", "r") as f:
             calorie_of_elf = 0
         else:
             calorie_of_elf += int(line)
-
-# Solution for 1st question.
-print(max(total_calorie_list)) # 71023
-
-#Solution for 2nd question.
-print(sum(sorted(total_calorie_list, reverse=True)[:3])) # 206289
+    print(max(total_calorie_list))
+    print(sum(sorted(total_calorie_list, reverse=True)[:3]))
